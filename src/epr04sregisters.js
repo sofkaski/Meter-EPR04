76a4550df0d5fe1b2b653c0c43d82df119e27615
Jyrki K <jyrkiokaski@gmail.com>
2016-09-04 16:06:47 +0300

    Corrected registers definition

--- src/epr04sregisters.js
+++ src/epr04sregisters.js
@@ -1,8 +1,8 @@
     // Registers are in the order starting from 20. Every entry consists of two registers. I.e the first entry is register 20
     // and the second is 22 and so on
-module.exports = {
-REGISTER_OFFSET: 20,
-Epr04sRegisters: [
+module.exports = {REGISTER_OFFSET: 20};
+
+module.exports.Epr04sRegisters = [
         {label: "L1 Active Power", multiplier: 0.1, unit: "Watt", des: 1}, 
         {label: "L2 Active Power", multiplier: 0.1, unit: "Watt", des: 1}, 
         {label: "L3 Active Power", multiplier: 0.1, unit: "Watt", des: 1},
@@ -23,8 +23,9 @@ Epr04sRegisters: [
         {label: "Average Inductive Cos Phi", multiplier: 0.001, unit: "", des: 0}, 
         {label: "Average Capacitive Cos Phi", multiplier: 0.001, unit: "", des: 0}, 
         {label: "Frequency", multiplier: 0.01, unit: "Hz", des: 0} 
-		], 
-REGISTER: {    
+		]; 
+
+module.exports.REGISTER = {    
         L1ActivePower:0, 
         L2ActivePower:2, 
         L3ActivePower:4, 
@@ -45,8 +46,10 @@ REGISTER: {
         AverageInductiveCosPhi:34, 
         AverageCapacitiveCosPhi:36, 
         Frequency:38 
-    },
-REGISTER_GROUPS: {
+    };
+
+var REGISTER = module.exports.REGISTER;
+module.exports.REGISTER_GROUPS = {
     ActivePower: {L1:REGISTER.L1ActivePower,L2:REGISTER.L2ActivePower,L3:REGISTER.L3ActivePower},
     ReactivePower: {L1:REGISTER.L1ReactivePower,L2:REGISTER.L2ReactivePower,L3:REGISTER.L3ReactivePower},
     ApparentPower: {L1:REGISTER.L1ApparentPower,L2:REGISTER.L2ApparentPower,L3:REGISTER.L3ApparentPower},
@@ -57,8 +60,7 @@ REGISTER_GROUPS: {
     TotalApparentPower: {TotalApparentPower: REGISTER.TotalApparentPower},
     AverageCosPhi: {Inductive: REGISTER.AverageInductiveCosPhi, Capacitive: REGISTER.AverageCapacitiveCosPhi},
     Frequency: {Frequency: REGISTER.Frequency}
-    }
-};
+    };
 
 module.exports.registerValue = function(register, reg1value, reg2value) {
         return (((reg1value<<16) | reg2value)*Epr04sRegisters[register].multiplier).toFixed(Epr04sRegisters[register].des);
